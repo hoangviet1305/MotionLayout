ac4bee09c65c91afa82c88b21c6d655f62d32f38
DESKTOP-U8PJLEN\DELL <hoangviet13052000@gmail.com>
2021-09-23 11:38:29 +0700

    finish shopping

---
+++ app/src/main/java/com/example/nguyenhoangviet_motionlayout/bt2.java
@@ -0,0 +1,14 @@
+package com.example.nguyenhoangviet_motionlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class bt2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bt2);
+    }
+}
